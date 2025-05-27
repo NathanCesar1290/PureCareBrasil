a8f16d80a3df43f7c02ab40728e799c212e2f397
Nathan Cesar <nathancesarsousamoraes@gmail.com>
2025-05-27 12:04:18 -0300

    Removendo duplicação da declaração de PORT

--- server.js
+++ server.js
@@ -257,15 +257,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(
-  PORT,
-  console.log(
-    `Servidor rodando em modo ${process.env.NODE_ENV} na porta ${PORT}`.yellow.bold
-  )
-);
-
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Erro: ${err.message}`.red);
